6dbb82d1214adaad5de3b053c0a75e7d411beb9c
alt5e <62925901+alt5e@users.noreply.github.com>
2021-04-22 15:15:46 +0700

    Create LoginController.java

---
+++ LEarn/src/learn/LoginController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learn;
+
+/**
+ *
+ * @author ASUS
+ */
+public class LoginController {
+    private Login model;
+    private LoginView view;
+
+    public LoginController(Login model, LoginView view) {
+        this.model = model;
+        this.view = view;
+    }
+    
+    
+}
